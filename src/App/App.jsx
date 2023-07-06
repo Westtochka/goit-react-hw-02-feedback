788fb901f3cb1815130bc800b9468f56faa116cb
Olga Boiko <Itnumber1@ukr.net>
2023-07-06 16:11:32 +0300

    add step 2

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,42 @@
-import Counter from "../components/Statistics/Statistics";
+import { Component } from 'react'
+import Statistics from 'components/Statistics/Statistics'
 
-export const App = () => {
+class App extends Component {
+  state = {
+    good: 10,
+    neutral: 8,
+    bad: 3
+} 
+// handleClickGood=() =>{
+//   // this.setState({good: 5})
+//   this.setState((prevState)=>{
+//     return {good: prevState.good+1}
+//   })
+// }
+// handleClickNeutral=() =>{
+//   this.setState((prevState)=>{
+//     return {neutral: prevState.neutral+1}
+//   })
+// }
+// handleClickBad=() =>{
+//   this.setState((prevState)=>{
+//     return {bad: prevState.bad+1}
+//   })
+// }
+countTotalFeedback = () => {
+  const total = this.state.good + this.state.neutral + this.state.bad;
+  return total;
+};
+
+countPositiveFeedbackPercentage=()=>{
+  const positivePercentage = this.state.good/(this.state.good+this.state.neutral+this.state.bad)*100;
+  if ((this.state.good+this.state.neutral+this.state.bad)>0) {return Math.round(positivePercentage)}
+  else{return 0 };
+}
+ 
+render() {
+  const total=this.countTotalFeedback();
+  const positivePercentage = this.countPositiveFeedbackPercentage();
   return (
     <div
       style={{
@@ -13,7 +49,24 @@ export const App = () => {
       }}
     >
      
-       <Counter/>
+       <Statistics 
+       good={this.state.good} 
+       neutral={this.state.neutral} 
+       bad={this.state.bad} 
+       total={total} 
+       positivePercentage={positivePercentage}
+     />
+          
+       
+       {/* <Section title="">
+       <FeedbackOptions options={} onLeaveFeedback={}></FeedbackOptions>
+       </Section>
+       <Section title="">
+       <Statistics good={state.good} neutral={state.neutral} bad={state.bad} total={countTotalFeedback}
+        positivePercentage={countPositiveFeedbackPercentage}></Statistics>
+       </Section> */}
+
     </div>
   );
-};
+}}
+export default App
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,48 +1,20 @@
-import { Component } from 'react'
-import { Title } from "./Statistics.styled";
-// import {Button} from
-
-class Counter extends Component {
-	state = {
-        good: 0,
-        neutral: 0,
-        bad: 0
-} 
-handleClickGood=() =>{
-  // this.setState({good: 5})
-  this.setState((prevState)=>{
-    return {good: prevState.good+1}
-  })
-}
-handleClickNeutral=() =>{
-  this.setState((prevState)=>{
-    return {neutral: prevState.neutral+1}
-  })
-}
-handleClickBad=() =>{
-  this.setState((prevState)=>{
-    return {bad: prevState.bad+1}
-  })
-}
-// countTotalFeedback=()=>this.setState((prevState)=>({total:this.state.good+this.state.neutral+this.state.bad}))
-
-render() {
-		return (<section>
-            <Title>Please leave feedback</Title>
-              <button onClick={this.handleClickGood}>Good</button>
-              <button onClick={this.handleClickNeutral}>Neutral</button>
-              <button onClick={this.handleClickBad}>Bad</button>
-            <Title>Statistics</Title>
-              <p>Good: {this.state.good}</p>
-              <p>Neutral: {this.state.neutral}</p>
-              <p>Bad: {this.state.bad}</p>
-              {/* <p>Total: {this.state.good+this.state.neutral+this.state.bad}</p>
-              <p>Positive feedback: {Math.round(this.state.good/(this.state.good+this.state.neutral+this.state.bad)*100)} %</p> */}
-          <p>Total:</p>
+const Statistics=({good, neutral, bad, total, positivePercentage}) =>{
+		return (
+          <section>
+            {/* <Title>Please leave feedback</Title>
+              <button onClick={handleClickGood}>Good</button>
+              <button onClick={handleClickNeutral}>Neutral</button>
+              <button onClick={handleClickBad}>Bad</button>
+            <Title>Statistics</Title> */}
+              <p>Good: {good}</p>
+              <p>Neutral: {neutral}</p>
+              <p>Bad: {bad}</p>
+              <p >Total:{total}</p>
+              <p >Positive feedback:{positivePercentage}  %</p>
           
           </section>
         )
-        }
+        
 }
 
-export default Counter
+export default Statistics
