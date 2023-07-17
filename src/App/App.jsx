5548c7c0f13e498453cce53580a65632be009386
Olga Boiko <Itnumber1@ukr.net>
2023-07-17 23:08:15 +0300

    finish

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,96 +1,73 @@
-import { Component } from 'react'
-import Statistics from 'components/Statistics/Statistics'
+import React, { Component } from 'react';
+import Statistics from 'components/Statistics/Statistics';
 import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
 import Section from 'components/Section/Section';
 import 'react-notifications/lib/notifications.css';
 
-// import { Notification } from 'notification';
 class App extends Component {
   state = {
     good: 0,
     neutral: 0,
     bad: 0
-} 
- handleClickGood=() =>{
-  // this.setState({good: 5})
-  this.setState((prevState)=>{
-    return {good: prevState.good+1}
-  })
-}
-handleClickNeutral=() =>{
-  this.setState((prevState)=>{
-    return {neutral: prevState.neutral+1}
-  })
-}
+  };
 
-handleClickBad=() =>{
-  this.setState((prevState)=>{
-    return {bad: prevState.bad+1}
-     })
-}
-countTotalFeedback = () => {
-  const total = this.state.good + this.state.neutral + this.state.bad;
-  return total;
-};
+  handleButtonClick = (option) => {
+    this.setState((prevState) => ({
+      [option]: prevState[option] + 1
+    }));
+  };
 
-countPositiveFeedbackPercentage=()=>{
-  const positivePercentage = this.state.good/
-  (this.state.good+this.state.neutral+this.state.bad)*100;
-  if ((this.state.good+this.state.neutral+this.state.bad)>0) 
-  {return Math.round(positivePercentage)}
-  else{return 0 };
-}
- 
-render() {
-  const options = Object.keys(this.state);
-  // eslint-disable-next-line no-unused-vars
-  const onLeaveFeedback={
-    onGood: this.handleClickGood,  
-    onNeutral: this.handleClickNeutral, 
-    onBad: this.handleClickBad
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    const { good } = this.state;
+    return total > 0 ? Math.round((good / total) * 100) : 0;
+  };
+
+  render() {
+    const options = Object.keys(this.state);
+
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+          backgroundColor: '#eeeee9',
+          padding: '50px 50px'
+        }}
+      >
+        <Section title="Statistics">
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.handleButtonClick}
+          />
+        </Section>
+        <Section title="Please leave feedback">
+          {this.countTotalFeedback() > 0 ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()} // Нужно исправить эту строку
+            />
+          ) : (
+            <h3 style={{ color: '#e01b1b' }}>There is no feedback</h3>
+          )}
+        </Section>
+      </div>
+    );
   }
-  
-  const total=this.countTotalFeedback();
-  const positivePercentage = this.countPositiveFeedbackPercentage();
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-        backgroundColor:'#eeeee9',
-        padding:'50px 50px'
-      }}
-    >
-     
-     {/* <Notification message="There is no feedback"/> */}
-         <Section title="Statistics">
-         <FeedbackOptions
-  options={options}
-  onLeaveFeedback={{
-    onGood: this.handleClickGood,
-    onNeutral: this.handleClickNeutral,
-    onBad: this.handleClickBad
-  }}
-/>
-</Section>   
-<Section title="Please leave feedback">
-     {this.countTotalFeedback() > 0 ? (
-       <Statistics 
-       good={this.state.good} 
-       neutral={this.state.neutral} 
-       bad={this.state.bad} 
-       total={total} 
-       positivePercentage={positivePercentage}
-     />):  <h3 style={{
-      color: '#e01b1b,'
-    }}>There is no feedback</h3> }
-     </Section>    
-</div>
-  );
-}}
-export default App
+}
+
+export default App;
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,42 +1,68 @@
+// import { Button } from "./FeedbackOptions.styled";
+// import PropTypes from 'prop-types'
+
+// const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+//   const { onGood, onNeutral, onBad } = onLeaveFeedback;
+
+//   return (
+//     <>
+//       <Button type="button" onClick={onGood}  >
+//         {options[0]}
+//       </Button>
+//       <Button type="button"
+//       style={{        
+//         color: '#48cef3'
+//       }}      
+//       onClick={onNeutral}>
+//         {options[1]}
+//       </Button>
+//       <Button type="button"
+//       style={{        
+//         color: '#747471'
+//       }}
+//        onClick={onBad}>
+//         {options[2]}
+//       </Button>
+//     </>
+//   );
+// };
+
+// export default FeedbackOptions;
+
+// FeedbackOptions.propTypes = {
+//   options: PropTypes.arrayOf(PropTypes.string).isRequired,
+//   onLeaveFeedback: PropTypes.shape({
+//     onGood: PropTypes.func.isRequired,
+//     onNeutral: PropTypes.func.isRequired,
+//     onBad: PropTypes.func.isRequired,
+//   }).isRequired,
+// };
+
+import React from 'react';
 import { Button } from "./FeedbackOptions.styled";
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-  const { onGood, onNeutral, onBad } = onLeaveFeedback;
+  const buttons = options.map((option) => (
+    <Button key={option} type="button" onClick={() => onLeaveFeedback(option)}>
+      {option}
+    </Button>
+  ));
 
-  return (
-    <>
-      <Button type="button" onClick={onGood}  >
-        {options[0]}
-      </Button>
-      <Button type="button"
-      style={{        
-        color: '#48cef3'
-      }}      
-      onClick={onNeutral}>
-        {options[1]}
-      </Button>
-      <Button type="button"
-      style={{        
-        color: '#747471'
-      }}
-       onClick={onBad}>
-        {options[2]}
-      </Button>
-    </>
-  );
+  return <>{buttons}</>;
 };
 
 export default FeedbackOptions;
 
 FeedbackOptions.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string).isRequired,
-  onLeaveFeedback: PropTypes.shape({
-    onGood: PropTypes.func.isRequired,
-    onNeutral: PropTypes.func.isRequired,
-    onBad: PropTypes.func.isRequired,
-  }).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
 };
+
+
+
+
+
 
 
 
