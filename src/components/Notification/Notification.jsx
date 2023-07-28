1e06f6987b9571365ab5ea4a6dc69361be79117d
Olga Boiko <Itnumber1@ukr.net>
2023-07-28 13:20:16 +0300

    edit notificationion

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,32 +1,14 @@
 // import css from './Notification.module.css';
-// import PropTypes from 'prop-types';
-// import React from 'react';
-
-// const Notification = ({ message }) => {
-//     return <p >{message}</p>
-// };
-
-// Notification.propTypes = {
-//     message: PropTypes.string.isRequired,
-// };
-
-// export default Notification;
-
-
-
-// ..................
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 
-class Notification extends Component {
-  render() {
-    const { message } = this.props;
-    return <p>{message}</p>;
-  }
-}
+const Notification = ({ message }) => {
+    return <p >{message}</p>
+};
 
 Notification.propTypes = {
-  message: PropTypes.string.isRequired,
+    message: PropTypes.string.isRequired,
 };
 
 export default Notification;
+
